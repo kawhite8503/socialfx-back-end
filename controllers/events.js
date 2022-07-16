3a7f311ada4801fc1b97ce691d9d14882f819eb1
Pierce Petit <ppetit2496@gmail.com>
2022-07-15 23:25:11 -0500

    build out show functionality and adddetails commit

--- controllers/events.js
+++ controllers/events.js
@@ -16,6 +16,36 @@ function create(req, res) {
   })
   console.log(req.body)
 }
+// function createDetails(req, res) {
+//   Event.findById(req.params.id)
+//     .then(event =>{
+//       event.guestList.push(req.body)
+//       event.activities.push(req.body)
+//       event.items.push(req.body)
+//       event.save()
+//       .then(() => {
+//         console.log(event.comments)
+//         console.log(event.activities)
+//         console.log(event.items)
+//       })
+//     })
+//     .then(populatedEvent => {
+//       res.json(populatedEvent)
+//     })
+//   })
+// }
+function show(req, res){
+  Event.findById(req.params.id)
+  .populate('owner')
+  .then(event => {
+    res.json(event)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({err: err.errmsg})
+  })
+}
 export {
-  create
+  create,
+  show,
 }
--- routes/events.js
+++ routes/events.js
@@ -10,5 +10,6 @@ const router = Router()
 /*---------- Protected Routes ----------*/
 router.use(decodeUserFromToken)
 router.post('/', checkAuth, eventsCtrl.create)
-
+//router.post('/:id/details',checkAuth, eventsCtrl.createDetails)
+router.get('/:id',eventsCtrl.show)
 export { router }
